057c05320e4ea26a7cf0e23ecad913b009c35a76
naoto <micuat@aim.com>
2014-04-24 17:49:17 -0400

    Planar segmentation example

---
+++ 01planar/src/ofApp.cpp
@@ -0,0 +1,129 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+	ofSetLogLevel(OF_LOG_VERBOSE);
+	
+	ofEnableDepthTest();
+	
+	kinect.setRegistration(true);
+	kinect.init();
+	kinect.open();
+	
+	ofSetFrameRate(30);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+	kinect.update();
+}
+
+inline ofxPCL::PointXYZRGBCloud getPointCloudFromKinect(ofxKinect &kinect, int step = 2) {
+	ofxPCL::PointXYZRGBCloud cloud(new ofxPCL::PointXYZRGBCloud::element_type);
+	
+	int count = 0;
+	int w = kinect.getWidth();
+	int h = kinect.getHeight();
+	
+	cloud->width = ceil((float)w / (float)step) * ceil((float)h / (float)step);
+	cloud->height = 1;
+	cloud->is_dense = false;
+	cloud->points.resize(cloud->width * cloud->height);
+	for(int y = 0; y < h; y += step) {
+		for(int x = 0; x < w; x += step) {
+			if(kinect.getDistanceAt(x, y) > 0) {
+				ofVec3f v = kinect.getWorldCoordinateAt(x, y);
+				cloud->points[count].x = v.x;
+				cloud->points[count].y = v.y;
+				cloud->points[count].z = v.z;
+				ofColor col = kinect.getColorAt(x, y);
+				uint32_t rgb = ((uint32_t)col.r << 16 | (uint32_t)col.g << 8 | (uint32_t)col.b);
+				cloud->points[count].rgb = *reinterpret_cast<float*>(&rgb);
+				count++;
+			}
+		}
+	}
+	cloud->points.resize(count);
+	cloud->width = count;
+	
+	return cloud;
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+	ofBackground(10);
+	
+	easyCam.begin();
+	
+	ofxPCL::PointXYZRGBCloud cloud = getPointCloudFromKinect(kinect, 8);
+	vector<ofxPCL::PointXYZRGBCloud> clouds = ofxPCL::segmentation(cloud, pcl::SACMODEL_PLANE, mouseX, 10, 1);
+	
+	ofMesh mesh;
+	
+	if( clouds.size() > 0 )
+		mesh = ofxPCL::toOF(clouds.at(0));
+	
+	ofPushMatrix();
+	// the projected points are 'upside down' and 'backwards'
+	ofScale(1, -1, -1);
+	ofTranslate(0, 0, -1000); // center the points a bit
+	
+	mesh.drawVertices();
+	
+	ofPopMatrix();
+	
+	easyCam.end();
+
+}
+
+//--------------------------------------------------------------
+void ofApp::exit(){
+	kinect.close();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+	if( key == ' ' ) {
+		toggleWireframe = !toggleWireframe;
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
